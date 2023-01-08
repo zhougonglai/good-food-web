70b866fe55b60fe7541a48a30ed84950510958c8
周公来 <zhougonglai@nn.com>
2023-01-08 22:57:15 +0800

    logo

--- components/App/Header.vue
+++ components/App/Header.vue
@@ -4,8 +4,9 @@
       class="inline-flex items-center justify-between shadow rounded-full backdrop-blur transition px-4 bg-white bg-opacity-80 backdrop-blur rounded-tr-3xl rounded-bl-3xl rounded-tl-xl rounded-br-xl">
       <div class="inline-flex items-end h-10">
         <img src="@/assets/icon/logo.svg" width="75" class="" />
-        <div class="text-green-500 text-xl ml-4 font-mono tracking-tight">
-          Kung Food
+        <div class="text-green-500 text-xl ml-4 font-mono tracking-tight leading-5 text-center">
+          Kung Food<br />
+          功夫美食
         </div>
       </div>
       <nav class="flex items-center mx-10 space-x-4 text-gray-500 h-14 leading-[56px]">
