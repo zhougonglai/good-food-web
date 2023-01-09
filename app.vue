e4f36e50ee21cee482b54824e371ca98ca35de91
周公来 <zhougonglai@nn.com>
2023-01-10 01:14:02 +0800

    bg

--- app.vue
+++ app.vue
@@ -4,6 +4,6 @@
     <template #header>
       <AppHeader />
     </template>
-    <NuxtPage class="antialiased bg-gradient-to-br from-green-200 via-white to-yellow-100 min-h-screen" />
+    <NuxtPage class="antialiased bg-slate-100 min-h-screen" />
   </NuxtLayout>
 </template>
